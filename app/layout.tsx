10176502dbc3a0a69294dcbe891767fa28cf1c2a
sabari245 <sabari2004@hotmail.com>
2025-05-03 22:25:26 +0530

    updated the pixelid

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1948143092656196');
+              fbq('init', '1214189476768948');
               fbq('track', 'PageView');
             `,
           }}
